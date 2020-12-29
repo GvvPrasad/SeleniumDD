9966a4e984d3cd5d4d9f39841bf929019ef1feee
prasad <varaprasad1819@gmail.com>
2020-12-29 11:24:35 +0530

    practice test script code removed

--- src/test/java/com/autom/testscripts/Partice.java
+++ src/test/java/com/autom/testscripts/Partice.java
@@ -14,13 +14,5 @@ import com.autom.utilities.ExcelUtil;
 @Listeners(com.autom.listeners.TestNGListener.class)
 public class Partice extends Base{
 	
-	@Test
-	public static void tab() throws Exception {
-		String filename = "D://workspace//SeleniumDD//src//test//resources//sam.xlsx";
-		ExcelUtil.getExcel(filename);
-		ExcelUtil.createSheet("sam3");
-		ExcelUtil.getSheet(0);
-		ExcelUtil.createRow(1);
-		ExcelUtil.closeExcel(filename);
-	}
+	
 }
